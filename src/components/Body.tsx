a91342183b598740937c3ff3393bafe07ab3321c
SEANDOUGHTYEDL <sean.doughty@mail.utoronto.ca>
2020-12-18 15:28:48 -0500

    adding http to url

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import InputForm from './InputForm';
 import axios from "axios"
 
-let url = 'flask-6s44:8080/download';
+let url = 'http://flask-6s44:8080/download';
 
 const Body = () => {
   // file URL state
