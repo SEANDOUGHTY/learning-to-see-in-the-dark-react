7ff837f3a21d4b1a2f72f65434bd147759f9163f
SEANDOUGHTYEDL <sean.doughty@mail.utoronto.ca>
2020-12-20 23:39:42 -0500

    updating favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const Header = (): JSX.Element => (
   <div className="header">
-  <div className="jumbotron">
-  <h1 className="display-4">Learning to See in Dark Inference</h1>
-  <p className="lead">Some text here...</p>
+    <div className="jumbotron">
+    <div className="content">
+  <h1>Learning to See in Dark Inference</h1>
+      <p>Some text here...</p>
+      <h3 className="display-6">Heading 1</h3>   
+      <p>Lorem ipsum dolor sit   amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac ut consequat semper viverra. Urna neque viverra justo nec ultrices. Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Pellentesque habitant morbi tristique senectus et netus et. Donec massa sapien faucibus et. A scelerisque purus semper eget duis at. Pellentesque adipiscing commodo elit at. Ultrices in iaculis nunc sed augue. Neque sodales ut etiam sit amet nisl purus in mollis. Faucibus turpis in eu mi. Integer enim neque volutpat ac tincidunt. Scelerisque fermentum dui faucibus in ornare. Ac auctor augue mauris augue neque. Tortor id aliquet lectus proin nibh.</p>
+      <h3>Heading 2</h3>      
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac ut consequat semper viverra. Urna neque viverra justo nec ultrices. Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Pellentesque habitant morbi tristique senectus et netus et. Donec massa sapien faucibus et. A scelerisque purus semper eget duis at. Pellentesque adipiscing commodo elit at. Ultrices in iaculis nunc sed augue. Neque sodales ut etiam sit amet nisl purus in mollis. Faucibus turpis in eu mi. Integer enim neque volutpat ac tincidunt. Scelerisque fermentum dui faucibus in ornare. Ac auctor augue mauris augue neque. Tortor id aliquet lectus proin nibh.</p>
+      <h3>Heading 3</h3>     
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac ut consequat semper viverra. Urna neque viverra justo nec ultrices. Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Pellentesque habitant morbi tristique senectus et netus et. Donec massa sapien faucibus et. A scelerisque purus semper eget duis at. Pellentesque adipiscing commodo elit at. Ultrices in iaculis nunc sed augue. Neque sodales ut etiam sit amet nisl purus in mollis. Faucibus turpis in eu mi. Integer enim neque volutpat ac tincidunt. Scelerisque fermentum dui faucibus in ornare. Ac auctor augue mauris augue neque. Tortor id aliquet lectus proin nibh.</p>
+        </div>
   </div>
   </div>
 );
